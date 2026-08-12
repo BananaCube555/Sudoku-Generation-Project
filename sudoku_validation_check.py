e906b963fdfb4de94600e73513d07ebfcdb2ca56
Bananas021k <bananas021k@gmail.com>
2026-08-12 22:26:15 +0300

    Box check

--- sudoku_validation_check.py
+++ sudoku_validation_check.py
@@ -38,5 +38,21 @@ def check_row(data):
     
     return True
 
+def check_box(data):
+    for row in range(0, 9, 3):
+        for col in range(0, 9, 3):
+
+            cash = []
+
+            for r in range(row, row + 3):
+                for c in range(col, col + 3):
+                    cash.append(data[r][c])
+
+            if len(cash) != len(set(cash)):
+                return False
+
+    return True
+
 check_row(EasySodukuData[1]["board"])
 check_col(EasySodukuData[1]["board"])
+check_box(EasySodukuData[1]["board"])
